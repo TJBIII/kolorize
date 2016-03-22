c7d6434ab52ce5f3061ac99e874264eb04644a7f
Thomas Buida <thomasbuida@gmail.com>
2016-03-22 15:08:29 -0500

    test template previews

--- app/controllers/PreviewCtrl.js
+++ app/controllers/PreviewCtrl.js
@@ -12,7 +12,54 @@ app.controller("PreviewCtrl",
 
     $scope.setTemplate = (str) => $scope.chosenTemplate = str;
 
-    
+    $scope.cycleColors = () => {
+      const node = document.getElementById('template-node');
+
+      let imgs = [];
+
+
+      domtoimage.toPng(node)
+          .then(function (dataUrl) {
+              var img = new Image();
+              img.src = dataUrl;
+              // imgs.push(img.src);
+              // $('#output').append(img);
+              imgs.push(img);
+
+              //change the navbar/jumbotron color and make the next preview
+              $('.navbar').css('backgroundColor', 'lightblue');
+              $('.templateBody').css('backgroundColor', 'white');
+              $('footer.page-footer').css('backgroundColor', 'pink');
+
+
+                  domtoimage.toPng(node)
+                      .then(function (dataUrl) {
+                          var img = new Image();
+                          img.src = dataUrl;
+                          // imgs.push(img.src);
+                          // console.log("imgs", imgs);
+                          // $('#output').append(img);
+
+                          imgs.push(img);
+
+                          //change back to starting values
+                          $('.navbar').css('backgroundColor', 'black');
+                          $('.jumbotron').css('backgroundColor', 'lightgrey');
+                          $('footer.page-footer').css('backgroundColor', 'orange');
+
+
+                          imgs.forEach((element) => $('#output').append(element));
+                      })
+                      .catch(function (error) {
+                          console.error('oops, something went wrong!', error);
+                      });
+              
+          })
+          .catch(function (error) {
+              console.error('oops, something went wrong!', error);
+          });
+
+    }
 
 
 
