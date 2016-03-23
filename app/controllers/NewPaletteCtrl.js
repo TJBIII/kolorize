5d696dc24992a09d141d2ec585e8dbb2ec4a7a60
Thomas Buida <thomasbuida@gmail.com>
2016-03-23 14:49:56 -0500

    Image upload to canvas

--- app/controllers/NewPaletteCtrl.js
+++ app/controllers/NewPaletteCtrl.js
@@ -49,6 +49,34 @@ app.controller("NewPaletteCtrl",
         (response) => console.log(response)  // Handle reject
       );
     };
+
+
+    // $(function () {
+      let imageLoader = $('#imageLoader');
+
+      imageLoader.change(handleImage);
+
+      let canvas = $('#imageCanvas')[0];
+      let ctx = canvas.getContext('2d');
+
+
+
+      function handleImage(e){
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var img = new Image();
+            img.onload = function(){
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img,0,0);
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);     
+      }
+    // })
+
+
   }
 
 ]);
