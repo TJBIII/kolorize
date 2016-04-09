b28d2a0e33e7358372fdf6c76ca59d36509ff477
Thomas Buida <thomasbuida@gmail.com>
2016-04-09 15:59:05 -0500

    new palette is saved with username attached

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -14,7 +14,7 @@ app.controller("LoginCtrl",
     let ref = new Firebase(firebaseURL);
 
     // Variables on $scope for use in DOM
-    $scope.account = { email: "", password: "", name: ""};
+    $scope.account = { email: "", password: "", name: "", userName: ""};
     $scope.message = "";
 
     /*
@@ -31,7 +31,7 @@ app.controller("LoginCtrl",
         } else {
           console.log(`Created user account with uid: ${authData.uid}`);
           //add the new user's information to firebase
-          let userObj = { uid: authData.uid, name: $scope.account.name}
+          let userObj = { uid: authData.uid, uName: $scope.account.userName}
           authFactory.storeUser(userObj);
           $scope.login();
         }
